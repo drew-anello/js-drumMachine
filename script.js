319389568b04083b0f1b3f069e16c84c9bfb45a0
Drew Anello <drew.anello@gmail.com>
2022-10-16 23:28:11 -0400

    allows user to hit keys as much as they want

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+const music = 
 window.addEventListener('keydown', function (e) {
 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 if(!audio) return;
-
+audio.currentTime = 0;
 audio.play();
 
 });
